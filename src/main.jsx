cab9d178c5cbcf8178a7517701beeea4325f53a6
MichaelYang-lyx <yang.5915@buckeyemail.osu.edu>
2023-10-31 00:17:28 +0800

    update something

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import GitHubCorner from "./Githubcorner";
+import GitHubCorner from "./GitHubCorner";
 import { AuthContextProvider } from "./context/AuthContext";
 import { ChatContextProvider } from "./context/ChatContext";
 
